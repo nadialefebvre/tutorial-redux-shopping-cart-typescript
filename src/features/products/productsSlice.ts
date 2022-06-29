67b81ae45bc93199545e9263b6a75320638e4221
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-29 11:37:35 +0200

    lessons 1-6 done

---
+++ src/features/products/productsSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Product } from "../../app/api";
+
+export interface ProductsState {
+  products: {[id: string]: Product}
+}
+
+const initialState: ProductsState = {
+  products: {
+    "123": {
+      name: "fake"
+    }
+  }
+}
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {}
+})
+
+export default productsSlice.reducer
